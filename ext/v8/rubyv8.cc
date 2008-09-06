c32e400caf1708bab50ad3b0f35934e04fc5fac8
Matthew Draper <matthew@trebex.net>
2008-09-06 14:22:31 +0930

    Raise JavaScript exceptions into Ruby.

--- ext/v8/rubyv8.cc
+++ ext/v8/rubyv8.cc
@@ -16,6 +16,18 @@
 # endif
 #endif
 
+#define NAMESIZE 200L
+
+#define PrepareScope \
+   static char _name[NAMESIZE] = ""; \
+   if (*_name == '\0') \
+      snprintf(_name, NAMESIZE, "%s:%s", __FILE__, __func__); \
+   RubyScope ruby_scope(_name)
+
+#define CheckScope \
+   ruby_scope.Check()
+
+
 using namespace v8;
 
 Persistent<Context> _context;
@@ -32,6 +44,43 @@ typedef struct {
 VALUE cContext;
 VALUE cRubyLandProxy;
 VALUE cJSLandProxyRef;
+
+static VALUE convert_js_to_ruby_string(Handle<Value> value);
+
+static Persistent<Message> last_message;
+static inline void set_last_message(Handle<Message> message = Local<Message>()) {
+   last_message.Dispose();
+   last_message = Persistent<Message>::New(message);
+}
+
+class RubyScope {
+public:
+   RubyScope(char* name_) : name(name_), context_scope(_context) {
+      try_catch.SetVerbose(true);
+   }
+   ~RubyScope() {
+      this->Check();
+   }
+
+   void Check() {
+      if (try_catch.HasCaught()) {
+         Handle<Value> exc = try_catch.Exception();
+         try_catch.Reset();
+
+         Handle<Message> msg = last_message;
+         set_last_message();
+
+         VALUE ruby_str = convert_js_to_ruby_string(exc);
+         rb_raise(rb_eRuntimeError, StringValueCStr(ruby_str));
+      }
+   }
+
+private:
+   const char * const name;
+   HandleScope handle_scope;
+   Context::Scope context_scope;
+   TryCatch try_catch;
+};
 
 static bool ruby_value_is_proxy(VALUE maybe_proxy) {
    return cRubyLandProxy == CLASS_OF(maybe_proxy);
@@ -101,21 +150,24 @@ static Handle<Object> make_js_land_proxy(VALUE value) {
    }
 }
 
-#define CB(name) reinterpret_cast<VALUE(*)(...)>(name)
 
 static VALUE convert_js_to_ruby_string(Handle<Value> value) {
+   PrepareScope;
    Handle<String> str = value->ToString();
+   CheckScope;
    int len = str->Length();
    char buffer[len];
    str->WriteAscii(buffer);
    return rb_str_new(buffer, len);
 }
 static Local<String> convert_ruby_to_js_string(VALUE inval) {
+   // FIXME: protect
    VALUE str = rb_funcall(inval, rb_intern("to_s"), 0);
    return String::New(StringValuePtr(str), StringValueLen(str));
 }
 
 static VALUE convert_js_to_ruby(Handle<Value> value) {
+   PrepareScope;
    check_js(value);
    if (value->IsUndefined() || value->IsNull()) return Qnil;
    if (value->IsTrue()) return Qtrue;
@@ -152,15 +204,11 @@ static Handle<Value> convert_ruby_to_js(VALUE value) {
    }
 }
 
-#define PrepareScope \
-   HandleScope handle_scope; \
-   Context::Scope context_scope(_context); \
-   TryCatch safe;
-
 static VALUE context_evaluate(VALUE UNUSED(self), VALUE source) {
    PrepareScope;
    Handle<String> str = convert_ruby_to_js_string(source);
    Handle<Value> result = Script::Compile(str)->Run();
+   CheckScope;
    return convert_js_to_ruby(result);
 }
 
@@ -170,7 +218,9 @@ static VALUE rubyland_to_s(VALUE self) {
 }
 static VALUE rubyland_inspect(VALUE self) {
    PrepareScope;
-   return convert_js_to_ruby_string(extract_from_ruby_land_proxy(self)->ToDetailString());
+   Handle<String> result = extract_from_ruby_land_proxy(self)->ToDetailString();
+   CheckScope;
+   return convert_js_to_ruby_string(result);
 }
 static VALUE rubyland_key_p(VALUE self, VALUE name) {
    PrepareScope;
@@ -182,7 +232,9 @@ static VALUE rubyland_delete(VALUE self, VALUE name) {
 }
 static VALUE rubyland_get(VALUE self, VALUE name) {
    PrepareScope;
-   return convert_js_to_ruby(extract_from_ruby_land_proxy(self)->Get(convert_ruby_to_js(name)));
+   Handle<Value> result = extract_from_ruby_land_proxy(self)->Get(convert_ruby_to_js(name));
+   CheckScope;
+   return convert_js_to_ruby(result);
 }
 static VALUE rubyland_set(VALUE self, VALUE name, VALUE value) {
    PrepareScope;
@@ -193,6 +245,7 @@ static VALUE rubyland_function_p(VALUE self) {
    return extract_from_ruby_land_proxy(self)->IsFunction() ? Qtrue : Qfalse;
 }
 static VALUE rubyland_respond_to_p(VALUE self, VALUE sym) {
+   PrepareScope;
    VALUE stringval = rb_funcall(sym, rb_intern("to_s"), 0);
    char* name = StringValuePtr(stringval);
 
@@ -237,9 +290,11 @@ static VALUE rubyland_native_call(int argc, VALUE* argv, VALUE self) {
       }
       result = func->Call(target->ToObject(), argc - 1, args);
    }
+   CheckScope;
    return convert_js_to_ruby(result);
 }
 static VALUE rubyland_method_missing(int argc, VALUE* argv, VALUE self) {
+   PrepareScope;
    if (argc < 1)
       rb_raise(rb_eArgError, "No method name given");
    VALUE func = rubyland_get(self, argv[0]);
@@ -253,47 +308,103 @@ static VALUE rubyland_method_missing(int argc, VALUE* argv, VALUE self) {
 }
 
 static Handle<Value> jsland_get(Local<String> name, const AccessorInfo& info) {
+   PrepareScope;
+   Handle<Value> real = info.This()->GetRealNamedPropertyInPrototypeChain(name);
+   if (!real.IsEmpty())
+      return real;
    VALUE self = extract_from_js_land_proxy(info.This());
    VALUE rname = convert_js_to_ruby_string(name);
-   return convert_ruby_to_js(rb_funcall(self, rb_intern(StringValueCStr(rname)), 0));
+   // FIXME: protect
+   VALUE func = rb_funcall(self, rb_intern("method"), 1, rname);
+   return convert_ruby_to_js(func);
 }
 static Handle<Value> jsland_set(Local<String> name, Local<Value> value, const AccessorInfo& info) {
+   PrepareScope;
    VALUE self = extract_from_js_land_proxy(info.This());
    VALUE rname = rb_funcall(convert_js_to_ruby_string(name), rb_intern("+"), rb_str_new2("="));
+
+   // FIXME: protect
    rb_funcall(self, rname, convert_js_to_ruby(value));
+
    return value;
 }
 static Handle<Boolean> jsland_has(Local<String> name, const AccessorInfo& info) {
+   PrepareScope;
    VALUE self = extract_from_js_land_proxy(info.This());
    VALUE rname = convert_js_to_ruby_string(name);
+   // FIXME: protect
    return RTEST(rb_funcall(self, rb_intern("respond_to?"), 1, rname)) ? True() : False();
 }
 static Handle<Boolean> jsland_delete(Local<String> name, const AccessorInfo& info) {
+   PrepareScope;
    VALUE self = extract_from_js_land_proxy(info.This());
    VALUE rname = convert_js_to_ruby_string(name);
+   // FIXME: protect
    return RTEST(rb_funcall(self, rb_intern("delete"), 1, rname)) ? True() : False();
 }
 static Handle<Array> jsland_enum(const AccessorInfo& info) {
+   PrepareScope;
    VALUE self = extract_from_js_land_proxy(info.This());
    return Handle<Array>::Cast(convert_ruby_to_js(rb_funcall(self, rb_intern("keys"), 0)));
 }
 static Handle<Value> jsland_call(const Arguments& args) {
+   PrepareScope;
    VALUE self = extract_from_js_land_proxy(args.This());
    int len = args.Length();
    VALUE ruby_args = rb_ary_new2(len);
    for (int i = 0; i < len; i++) {
       rb_ary_store(ruby_args, i, convert_js_to_ruby(args[i]));
    }
-   return convert_ruby_to_js(rb_apply(self, rb_intern("call"), ruby_args));
+   // FIXME: protect
+   ID sym = args.IsConstructCall() ? rb_intern("new") : rb_intern("call");
+   return convert_ruby_to_js(rb_apply(self, sym, ruby_args));
 }
 
+static void v8_msg(Handle<Message> message, Handle<Value> UNUSED(data)) {
+   set_last_message(message);
+}
+static NORETURN(void) v8_fatal(const char* location, const char* message) {
+   printf("\nV8 FATAL at %s:\n %s\n\n", location, message);
+   rb_exit(EXIT_FAILURE);
+}
+
+static VALUE context_method_missing(int argc, VALUE* argv, VALUE self) {
+   PrepareScope;
+   VALUE global = convert_js_to_ruby(_context->Global());
+   return rb_funcall3(global, SYM2ID(argv[0]), argc - 1, argv + 1);
+}
+
+static Handle<Value> js_get_func(Local<String> property, const AccessorInfo& info) {
+   return info.Data();
+}
+static void define_js_method(Handle<ObjectTemplate> tmpl, const char* name, 
+      Handle<Value> (*func)(const Arguments& args)) {
+
+   Local<String> jname = String::New(name);
+   Local<FunctionTemplate> t = FunctionTemplate::New();
+   t->SetCallHandler(func);
+   Local<Function> f = t->GetFunction();
+   f->SetName(jname);
+   tmpl->SetAccessor(jname, js_get_func, NULL, f, DEFAULT, ReadOnly);
+}
+
+static Handle<Value> js_toString(const Arguments& args) {
+   VALUE self = extract_from_js_land_proxy(args.This());
+   return convert_ruby_to_js_string(self);
+}
+
+#define CB(name) reinterpret_cast<VALUE(*)(...)>(name)
 extern "C" void Init_rubyv8() {
    VALUE johnson = rb_define_module("Johnson");
 
    VALUE v8 = rb_define_module_under(johnson, "V8");
    cContext = rb_define_class_under(v8, "Context", rb_cObject);
    rb_define_method(cContext, "evaluate", CB(context_evaluate), 1);
+   rb_define_method(cContext, "method_missing", CB(context_method_missing), -1);
    rb_iv_set(cContext, "@proxied_ruby_objects", rb_hash_new());
+
+   rb_define_const(v8, "VERSION",
+      rb_obj_freeze(rb_str_new2(V8::GetVersion())));
 
    cJSLandProxyRef = rb_define_class_under(v8, "JSLandProxyRef", rb_cObject);
 
@@ -311,6 +422,8 @@ extern "C" void Init_rubyv8() {
    rb_define_method(cRubyLandProxy, "native_call", CB(rubyland_native_call), -1);
    rb_define_method(cRubyLandProxy, "method_missing", CB(rubyland_method_missing), -1);
 
+   V8::AddMessageListener(v8_msg);
+   V8::SetFatalErrorHandler(v8_fatal);
 
    HandleScope handle_scope;
    _context = Context::New();
@@ -323,5 +436,7 @@ extern "C" void Init_rubyv8() {
    tJSLandProxy->InstanceTemplate()->SetInternalFieldCount(1);
    tJSLandProxy->InstanceTemplate()->SetNamedPropertyHandler(jsland_get, jsland_set, jsland_has, jsland_delete, jsland_enum);
    tJSLandProxy->InstanceTemplate()->SetCallAsFunctionHandler(jsland_call);
+
+   define_js_method(tJSLandProxy->PrototypeTemplate(), "toString", js_toString);
 }
 
